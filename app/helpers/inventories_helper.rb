429d7e377f504e8fdaee95ebef33bb62c15bfe56
Marco <mgenova03@gmail.com>
2018-06-15 11:37:12 -0400

    add quantity percentages

--- app/helpers/inventories_helper.rb
+++ app/helpers/inventories_helper.rb
@@ -8,4 +8,37 @@ module InventoriesHelper
     end
   end
 
+  def tuesday_quantity_needed(invent)
+    percent = invent.amount * 0.01
+    
+    if invent.product.measurement == '%'
+      if percent <= 0.25
+        1
+      else 
+        0
+      end
+    else 
+      invent.product.tuesday_max - invent.amount
+    end
+    
+  end
+
+  def thursday_quantity_needed(invent)
+    if invent.product.measurement == '%'
+      0
+    else 
+      invent.product.thursday_max - invent.amount
+    end
+  end
+
+
+  def add_percent(invent)
+    if invent.product.measurement == '%'
+      number_to_percentage(invent.amount, precision: 0)
+    else 
+      invent.amount
+    end
+  end
+
+
 end
