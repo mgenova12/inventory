9e8207d72f916ff555c067110f275ed890b91b98
Marco <mgenova03@gmail.com>
2018-06-22 15:40:30 -0400

    add case amount calculation

--- app/helpers/inventories_helper.rb
+++ app/helpers/inventories_helper.rb
@@ -1,17 +1,12 @@
 module InventoriesHelper
 
-  def round_to_zero(num)
-    if num < 0 
-      0
-    else 
-      num 
-    end
-  end
-
   def tuesday_quantity_needed(invent)
     percent = invent.amount * 0.01
     
-    if invent.product.measurement == '%'
+    if invent.product.case_quantity
+      case_amount = invent.product.thursday_max - invent.amount
+      case_amount / invent.product.case_quantity
+    elsif invent.product.measurement == '%'
       if percent <= 0.25
         1
       else 
@@ -24,7 +19,10 @@ module InventoriesHelper
   end
 
   def thursday_quantity_needed(invent)
-    if invent.product.measurement == '%'
+    if invent.product.case_quantity
+      case_amount = invent.product.thursday_max - invent.amount
+      case_amount / invent.product.case_quantity
+    elsif invent.product.measurement == '%'
       0
     else 
       invent.product.thursday_max - invent.amount
