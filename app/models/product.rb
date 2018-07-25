895a6ef7a821e7f492a599d128cd2685b94f9cdc
Marco <mgenova03@gmail.com>
2018-07-25 14:02:24 -0400

    prevent quantity from being 0

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
+  # validates :case_quantity, numericality: { other_than: 0 }
+
+  validate :non_zero
+
+  def non_zero
+    if self.case_quantity == 0
+      self.errors.add(:case_quantity, "Field can't be zero")
+    end
+  end
 
 end
--- app/models/trappe_product.rb
+++ app/models/trappe_product.rb
@@ -1,2 +1,10 @@
 class TrappeProduct < ApplicationRecord
+  validate :non_zero
+
+  def non_zero
+    if self.case_quantity == 0
+      self.errors.add(:case_quantity, "Field can't be zero")
+    end
+  end  
+
 end
